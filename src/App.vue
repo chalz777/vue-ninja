b028a2170d67b68016066c529780f9a1f5901600
chalz777 <charlesnam37@hotmail.com>
2020-12-10 18:10:28 -0800

    elint fix

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,6 @@
       </div>
 
       <v-spacer></v-spacer>
-
       <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,93 +1,89 @@
 <template>
   <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
+      <v-row class="text-center">
+          <v-col cols="12">
+              <v-img :src="require('../assets/logo.svg')"
+                     class="my-3"
+                     contain
+                     height="200" />
+          </v-col>
 
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
+          <v-col class="mb-4">
+              <h1 class="display-2 font-weight-bold mb-3">
+                  Welcome to Vuetify
+              </h1>
 
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
+              <p class="subheading font-weight-regular">
+                  For help and collaboration with other Vuetify developers,
+                  <br>please join our online
+                  <a href="https://community.vuetifyjs.com"
+                     target="_blank">Discord Community</a>
+              </p>
+          </v-col>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
+          <v-expansion-panels>
+              <v-expansion-panel v-for="(item,i) in 5"
+                                 :key="i">
+                  <v-expansion-panel-header>
+                      Item
+                  </v-expansion-panel-header>
+                  <v-expansion-panel-content>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </v-expansion-panel-content>
+              </v-expansion-panel>
+          </v-expansion-panels>
 
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
+          <v-col class="mb-5"
+                 cols="12">
+              <h2 class="headline font-weight-bold mb-3">
+                  What's next? asdasdasd
+              </h2>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
+              <v-row justify="center">
+                  <a v-for="(next, i) in whatsNext"
+                     :key="i"
+                     :href="next.href"
+                     class="subheading mx-3"
+                     target="_blank">
+                      {{ next.text }}
+                  </a>
+              </v-row>
+          </v-col>
 
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
+          <v-col class="mb-5"
+                 cols="12">
+              <h2 class="headline font-weight-bold mb-3">
+                  Important Links
+              </h2>
 
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
+              <v-row justify="center">
+                  <a v-for="(link, i) in importantLinks"
+                     :key="i"
+                     :href="link.href"
+                     class="subheading mx-3"
+                     target="_blank">
+                      {{ link.text }}
+                  </a>
+              </v-row>
+          </v-col>
 
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
+          <v-col class="mb-5"
+                 cols="12">
+              <h2 class="headline font-weight-bold mb-3">
+                  Ecosystem
+              </h2>
+
+              <v-row justify="center">
+                  <a v-for="(eco, i) in ecosystem"
+                     :key="i"
+                     :href="eco.href"
+                     class="subheading mx-3"
+                     target="_blank">
+                      {{ eco.text }}
+                  </a>
+              </v-row>
+          </v-col>
+      </v-row>
   </v-container>
 </template>
 
